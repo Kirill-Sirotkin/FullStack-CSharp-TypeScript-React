f3bf3ee1193e950b6c48166b7311f63b4ee9795c
Samcolserra <samcolserra@gmail.com>
2023-08-28 14:19:44 +0300

    Fixed Auth endpoint for more consistency

--- backend/WebApi.Controller/src/Controllers/AuthController.cs
+++ backend/WebApi.Controller/src/Controllers/AuthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApi.Service;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
